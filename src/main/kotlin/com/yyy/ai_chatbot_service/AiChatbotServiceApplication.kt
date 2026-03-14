25c11967073cd771bfe4c5df3a36a3a45bd48359
yuin <dldmldlsy@naver.com>
2026-03-14 20:28:21 +0900

    init spring boot project

---
+++ src/main/kotlin/com/yyy/ai_chatbot_service/AiChatbotServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.yyy.ai_chatbot_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiChatbotServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiChatbotServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/yyy/ai_chatbot_service/AiChatbotServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yyy.ai_chatbot_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiChatbotServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
